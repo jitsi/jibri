99a31734115fa1be6136d30083de1b6f92de21b6
Aaron van Meerten <avanmeerten@atlassian.com>
2017-03-09 17:22:52 -0600

    return code handling stub for pjsua

--- scripts/start-pjsua.sh
+++ scripts/start-pjsua.sh
@@ -17,3 +17,10 @@ LOG_FILE=/tmp/jibri-pjsua.log
 export DISPLAY=:1
 
 pjsua --capture-dev=$CAPTURE_DEV --playback-dev=$PLAYBACK_DEV --config-file $CONFIG_FILE sip:$SIP_ADDRESS --log-file=$LOG_FILE
+RETURN=$?
+
+if [ "$RETURN" -eq 0 ]; then
+    #graceful exit, so don't write to error
+else
+    #do something to restart?
+fi
