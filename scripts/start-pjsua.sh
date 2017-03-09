77c96c8204d6fc801b7a06c1e92cb83a88b20a5d
Saúl Ibarra Corretgé <saghul@gmail.com>
2017-03-09 22:25:29 +0100

    Add ability to set the SIP display name in pjsua

--- scripts/start-pjsua.sh
+++ scripts/start-pjsua.sh
@@ -4,6 +4,7 @@
 
 
 SIP_ADDRESS=$1
+DISPLAY_NAME="${2:-Meeting Room}"
 
 CONFIG_FILE=/home/jibri/pjsua.config
 
@@ -19,6 +20,7 @@ export DISPLAY=:1
 pjsua \
     --capture-dev=$CAPTURE_DEV \
     --playback-dev=$PLAYBACK_DEV \
+    --id "$DISPLAY_NAME <sip:jibri@127.0.0.1>" \
     --config-file $CONFIG_FILE
     --log-file=$LOG_FILE \
     sip:$SIP_ADDRESS
