0e8dec521e73b202948c26834a1f21f258f7454a
brian baldino <bbaldino@gmail.com>
2018-01-25 11:23:57 -0800

    tweak the interface

--- src/main/java/org/jitsi/jibri/capture/ffmpeg/executors/FfmpegExecutor.kt
+++ src/main/java/org/jitsi/jibri/capture/ffmpeg/executors/FfmpegExecutor.kt
@@ -1,10 +1,9 @@
 package org.jitsi.jibri.capture.ffmpeg.executors
 
 import org.jitsi.jibri.sink.Sink
+import org.jitsi.jibri.util.MonitorableProcess
 
-interface FfmpegExecutor {
+interface FfmpegExecutor : MonitorableProcess {
     fun launchFfmpeg(ffmpegExecutorParams: FfmpegExecutorParams, sink: Sink)
-    fun isAlive(): Boolean
-    fun getExitCode(): Int?
     fun stopFfmpeg()
 }
