9bf56d3d53d48508047ef78d0b9dbc32e1b6c5af
brian baldino <bbaldino@gmail.com>
2018-02-15 15:09:39 -0800

    create jar dir dynamically since it's empty

--- resources/debian-package/create_debian_package.sh
+++ resources/debian-package/create_debian_package.sh
@@ -11,6 +11,7 @@ JIBRI_JAR_PATH=$1
 echo "Copying $JIBRI_JAR_PATH into package location"
 
 # Copy the built jar into the expected location
+mkdir -p jibri/opt/jitsi/jibri
 cp $JIBRI_JAR_PATH jibri/opt/jitsi/jibri/
 
 # Build the package
