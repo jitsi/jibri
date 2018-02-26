cec396e80844072ef970a5d242ffe21170de9bc2
brian baldino <bbaldino@gmail.com>
2018-02-26 13:29:42 -0800

    add sample finalize recordings script

---
+++ resources/finalize_recording.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "This is a dummy finalize script"
+echo "You should put any finalize logic"
+echo "(renaming, uploading to a service"
+echo "or storage provider, etc.) here"
+
+exit 0
