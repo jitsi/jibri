3d9aaa9fadcad0a10f6d90ccbd1f68b5e30ecd67
brian baldino <bbaldino@gmail.com>
2018-02-26 13:47:40 -0800

    add underscore separator between callname and date in recordings filename

--- src/main/java/org/jitsi/jibri/sink/impl/FileSink.kt
+++ src/main/java/org/jitsi/jibri/sink/impl/FileSink.kt
@@ -17,7 +17,7 @@ class FileSink(val recordingsDirectory: File, callName: String, extension: Strin
     init {
         val currentTime = LocalDateTime.now()
         val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm-ss")
-        val filename = callName + currentTime.format(formatter) + extension
+        val filename = "${callName}_${currentTime.format(formatter)}$extension"
         if (recordingsDirectory.mkdirs() or recordingsDirectory.isDirectory) {
             file = File(recordingsDirectory, filename)
             logger.info("Using recording file " + file.toString())
