0e61273356bfc347ca479aba5d42f57394c839b0
brian baldino <bbaldino@gmail.com>
2018-03-13 14:27:30 -0700

    move JibriSeleniumOptions into JibriSelenium

--- src/main/kotlin/org/jitsi/jibri/selenium/JibriSelenium.kt
+++ src/main/kotlin/org/jitsi/jibri/selenium/JibriSelenium.kt
@@ -17,6 +17,7 @@
 
 package org.jitsi.jibri.selenium
 
+import org.jitsi.jibri.CallParams
 import org.jitsi.jibri.CallUrlInfo
 import org.jitsi.jibri.selenium.pageobjects.CallPage
 import org.jitsi.jibri.selenium.pageobjects.HomePage
@@ -31,6 +32,32 @@ import java.util.concurrent.ScheduledExecutorService
 import java.util.concurrent.ScheduledFuture
 import java.util.concurrent.TimeUnit
 import java.util.logging.Logger
+
+/**
+ * Options that can be passed to [JibriSelenium]
+ */
+data class JibriSeleniumOptions(
+    /**
+     * The parameters necessary for joining a call
+     */
+    val callParams: CallParams,
+    /**
+     * Which display selenium should be started on
+     */
+    val display: String = ":0",
+    /**
+     * The display name that should be used for jibri.  Note that this
+     * is currently only used in the sipgateway gateway scenario; when doing
+     * recording the jibri is 'invisible' in the call
+     */
+    val displayName: String = "",
+    /**
+     * The email that should be used for jibri.  Note that this
+     * is currently only used in the sipgateway gateway scenario; when doing
+     * recording the jibri is 'invisible' in the call
+     */
+    val email: String = ""
+)
 
 /**
  * The [JibriSelenium] class is responsible for all of the interactions with
