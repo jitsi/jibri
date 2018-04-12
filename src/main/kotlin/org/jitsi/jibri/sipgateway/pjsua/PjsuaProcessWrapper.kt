0af86cc5f853444ba38a1cc9a45eef846747727e
brian baldino <bbaldino@gmail.com>
2018-04-11 18:47:38 -0700

    add PjsuaProcessWrapper and refactor PjsuaExecutor to use it

---
+++ src/main/kotlin/org/jitsi/jibri/sipgateway/pjsua/PjsuaProcessWrapper.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright @ 2018 Atlassian Pty Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package org.jitsi.jibri.sipgateway.pjsua
+
+import org.jitsi.jibri.util.ProcessWrapper
+
+enum class PjsuaStatus {
+    HEALTHY,
+    ERROR
+}
+
+class PjsuaProcessWrapper(
+    command: List<String>,
+    environment: Map<String, String>,
+    processBuilder: ProcessBuilder = ProcessBuilder()
+) : ProcessWrapper<PjsuaStatus>(command, environment, processBuilder) {
+
+    override fun getStatus(): Pair<PjsuaStatus, String> {
+        val mostRecentLine = getMostRecentLine()
+        return Pair(PjsuaStatus.HEALTHY, mostRecentLine)
+    }
+}
--- src/main/kotlin/org/jitsi/jibri/sipgateway/pjsua/executor/PjsuaExecutor.kt
+++ src/main/kotlin/org/jitsi/jibri/sipgateway/pjsua/executor/PjsuaExecutor.kt
@@ -2,17 +2,16 @@ package org.jitsi.jibri.sipgateway.pjsua.executor
 
 import org.jitsi.jibri.sipgateway.SipClientParams
 import org.jitsi.jibri.sipgateway.pjsua.PjsuaFileHandler
+import org.jitsi.jibri.sipgateway.pjsua.PjsuaProcessWrapper
+import org.jitsi.jibri.sipgateway.pjsua.PjsuaStatus
 import org.jitsi.jibri.util.MonitorableProcess
 import org.jitsi.jibri.util.NameableThreadFactory
-import org.jitsi.jibri.util.Tee
 import org.jitsi.jibri.util.extensions.debug
 import org.jitsi.jibri.util.extensions.error
-import org.jitsi.jibri.util.stopProcess
-import java.io.BufferedReader
-import java.io.IOException
-import java.io.InputStreamReader
+import org.jitsi.jibri.util.logStream
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 import java.util.logging.Logger
 
 data class PjsuaExecutorParams(
@@ -30,19 +29,15 @@ private const val CONFIG_FILE_LOCATION = "/home/jibri/pjsua.config"
 private const val X_DISPLAY = ":1"
 
 class PjsuaExecutor(
-    private val executor: ExecutorService = Executors.newSingleThreadExecutor(NameableThreadFactory("pjsua"))
+    private val executor: ExecutorService = Executors.newSingleThreadExecutor(NameableThreadFactory("pjsua")),
+    private val processBuilder: ProcessBuilder = ProcessBuilder()
 ) : MonitorableProcess {
     private val logger = Logger.getLogger(this::class.qualifiedName)
     private val pjsuaOutputLogger = Logger.getLogger("pjsua")
     /**
      * The currently active (if any) pjsua process
      */
-    var currentPjsuaProc: Process? = null
-    /**
-     * In order to both analyze Pjsua's output live and log it to a file, we'll
-     * tee its stdout stream so that both consumers can get all of its output.
-     */
-    private var pjsuaOutputTee: Tee? = null
+    private var currentPjsuaProc: PjsuaProcessWrapper? = null
 
     init {
         pjsuaOutputLogger.useParentHandlers = false
@@ -52,66 +47,71 @@ class PjsuaExecutor(
      * Launch pjsua with the given [PjsuaExecutorParams]
      */
     fun launchPjsua(pjsuaExecutorParams: PjsuaExecutorParams): Boolean {
-        //TODO: not able to format this as nicely because we need arguments like
-        // "--id xx xx" to be parsed as 2 separate arguments when passing them
-        // to the shell ("--id" is one, "xx xx" is another); splitting on spaces
-        // doesn't work because the value might have a space in it.  look into
-        // a nicer way to do this?
-        val pjsuaCommand = """
-            pjsua
-            --capture-dev=$CAPTURE_DEVICE
-            --playback-dev=$PLAYBACK_DEVICE
-            --id
-            ${pjsuaExecutorParams.sipClientParams.displayName} <sip:jibri@127.0.0.1>
-            --config-file
-            $CONFIG_FILE_LOCATION
-            --log-file
-            /tmp/pjsua.out
-            sip:${pjsuaExecutorParams.sipClientParams.sipAddress}
-        """.trimIndent()
+        val command = listOf(
+            "pjsua",
+            "--capture-dev=$CAPTURE_DEVICE",
+            "--playback-dev=$PLAYBACK_DEVICE",
+            "--id", "${pjsuaExecutorParams.sipClientParams.displayName} <sip:jibri@127.0.0.1>",
+            "--config-file", CONFIG_FILE_LOCATION,
+            "--log-file", "/tmp/pjsua.out",
+            "sip:${pjsuaExecutorParams.sipClientParams.sipAddress}"
+        )
 
-        val command = pjsuaCommand.split("\n")
-        val pb = ProcessBuilder(command)
-        pb.redirectErrorStream(true)
-        pb.environment().put("DISPLAY", X_DISPLAY)
-
-        logger.info("Running pjsua command:\n ${pb.command()}")
         try {
-            currentPjsuaProc = pb.start()
-        } catch (e: IOException) {
-            logger.error("Error starting pjsua: $e")
+            currentPjsuaProc = PjsuaProcessWrapper(
+                command,
+                mapOf("DISPLAY" to X_DISPLAY),
+                processBuilder)
+        } catch (t: Throwable) {
+            logger.error("Error starting pjsua: $t")
             return false
         }
-        currentPjsuaProc?.let {
-            pjsuaOutputTee = Tee(it.inputStream)
+        return currentPjsuaProc?.let {
+            it.start()
+            logStream(it.getOutput(), pjsuaOutputLogger, executor)
+            true
+        } ?: run {
+            false
         }
-        pjsuaOutputTee?.let { tee ->
-            executor.submit {
-                val reader = BufferedReader(InputStreamReader(tee.addBranch()))
-                while (true) {
-                    pjsuaOutputLogger.info(reader.readLine())
-                }
-            }
-        }
-        logger.debug("Launched pjsua, is it alive? ${currentPjsuaProc?.isAlive}")
-        return true
     }
 
     /**
      * Shutdown pjsua gracefully
      */
     fun stopPjsua() {
-        stopProcess(currentPjsuaProc, "pjsua", logger)
+        logger.info("Stopping pjsua process")
+        currentPjsuaProc?.apply {
+            stop()
+            waitFor(10, TimeUnit.SECONDS)
+            if (isAlive) {
+                logger.error("Pjsua didn't stop, killing pjsua")
+                destroyForcibly()
+            }
+        }
+        logger.info("Pjsua exited with value ${currentPjsuaProc?.exitValue()}")
     }
 
     override fun getExitCode(): Int? {
-        currentPjsuaProc?.let {
-            return if (it.isAlive) null else it.exitValue()
+        return currentPjsuaProc?.let {
+            if (it.isAlive) null else it.exitValue()
         }
-        return null
     }
 
     override fun isHealthy(): Boolean {
-        return true
+        return currentPjsuaProc?.let {
+            val (status, mostRecentOutput) = it.getStatus()
+            return@let when (status) {
+                PjsuaStatus.HEALTHY -> {
+                    logger.debug("Pjsua appears healthy: $mostRecentOutput")
+                    true
+                }
+                PjsuaStatus.ERROR -> {
+                    logger.debug("Pjsua had an error: $mostRecentOutput")
+                    false
+                }
+            }
+        } ?: run {
+            false
+        }
     }
 }
