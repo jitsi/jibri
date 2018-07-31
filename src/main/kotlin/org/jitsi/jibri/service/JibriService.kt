41a637126c385384bfef0164cf1115ca4a1bb2b1
brian baldino <bbaldino@gmail.com>
2018-07-31 11:39:55 -0700

    define new AppData structure and add it to ServiceParams
    this will be used to allow passing of arbitrary data between the
    original creator of the JibriIq (i.e. the jitsi meet client) and Jibri,
    so we don't have to modify jitsi-protocol-jabber/jicofo every time.

--- src/main/kotlin/org/jitsi/jibri/service/JibriService.kt
+++ src/main/kotlin/org/jitsi/jibri/service/JibriService.kt
@@ -17,6 +17,7 @@
 
 package org.jitsi.jibri.service
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import org.jitsi.jibri.util.StatusPublisher
 
 enum class JibriServiceStatus {
@@ -25,10 +26,25 @@ enum class JibriServiceStatus {
 }
 
 /**
+ * Arbitrary Jibri specific data that can be passed in the
+ * [JibriIq#appData] field.  This entire structure will be parsed
+ * from a JSON-encoded string (the [JibriIq#appData] field).
+ */
+data class AppData(
+    /**
+     * A JSON-encoded string representing arbitrary data to be written
+     * to the metadata file when doing a recording.
+     */
+    @JsonProperty("file_recording_metadata")
+    val fileRecordingMetadata: String?
+)
+
+/**
  * Parameters needed for starting any [JibriService]
  */
 data class ServiceParams(
-    val usageTimeoutMinutes: Int
+    val usageTimeoutMinutes: Int,
+    val appData: AppData? = null
 )
 
 typealias JibriServiceStatusHandler = (JibriServiceStatus) -> Unit
