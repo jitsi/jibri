3df1c9c0b720d5e0696ba2cd49b728c2f6c39eef
Brian Baldino <bbaldino@gmail.com>
2018-12-20 14:59:35 -0800

    remove monitorableprocess. no longer used

--- src/main/kotlin/org/jitsi/jibri/capture/Capturer.kt
+++ src/main/kotlin/org/jitsi/jibri/capture/Capturer.kt
@@ -17,15 +17,12 @@
 
 package org.jitsi.jibri.capture
 
-import org.jitsi.jibri.util.MonitorableProcess
 import org.jitsi.jibri.sink.Sink
 
 class UnsupportedOsException(override var message: String = "Jibri does not support this OS") : Exception()
 
 /**
- * [Capturer] represents a process which will capture media.  It implements
- * [MonitorableProcess] so that its state can be monitored so it can be
- * restarted if it dies.
+ * [Capturer] represents a process which will capture media.
  */
 interface Capturer {
     /**
