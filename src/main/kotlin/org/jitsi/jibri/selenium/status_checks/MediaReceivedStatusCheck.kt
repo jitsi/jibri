3bdd290f745db0e6775e55cfe613456b5e678942
Brian Baldino <bbaldino@gmail.com>
2019-08-07 10:20:19 -0700

    refactor tracking of transiton to all muted state

--- src/main/kotlin/org/jitsi/jibri/selenium/status_checks/MediaReceivedStatusCheck.kt
+++ src/main/kotlin/org/jitsi/jibri/selenium/status_checks/MediaReceivedStatusCheck.kt
@@ -21,8 +21,8 @@ class MediaReceivedStatusCheck(
     // The last timestamp where we saw non-zero media.  We default with the
     // assumption we're receiving media.
     private var timeOfLastMedia = clock.instant()
-    // The timestamp at which we last saw that all clients were muted
-    private var firstTimeAllClientsMuted: Instant? = null
+    // The timestamp at which we last saw that all clients transitioned to muted
+    private val clientsAllMutedTransitionTime = StateTransitionTimeTracker(clock)
 
     override fun run(callPage: CallPage): SeleniumEvent? {
         val now = clock.instant()
@@ -30,34 +30,24 @@ class MediaReceivedStatusCheck(
         // getNumParticipants includes Jibri, so subtract 1
         val allClientsMuted = callPage.numRemoteParticipantsMuted() == (callPage.getNumParticipants() - 1)
         logger.info("Jibri client receive bitrates: $bitrates, all clients muted? $allClientsMuted")
-        if (allClientsMuted && firstTimeAllClientsMuted == null) {
-            firstTimeAllClientsMuted = now
-        } else if (!allClientsMuted) {
-            firstTimeAllClientsMuted = null
-        }
-        // There are a couple possible outcomes here:
-        // 1) downloadBitrate == 0 and not all the participants in the call are muted (meaning
-        //     we *should* be receiving media) --> Fire SeleniumEvent.NoMediaReceived
-        // 2) downloadBitrate == 0 and all clients are muted, BUT this has been the case for too
-        //     long and we're going to timeout --> Fire SeleniumEvent.CallEmpty
+        clientsAllMutedTransitionTime.maybeUpdate(allClientsMuted)
         val downloadBitrate = bitrates.getOrDefault("download", 0L) as Long
         if (downloadBitrate != 0L) {
             timeOfLastMedia = now
         }
         val timeSinceLastMedia = Duration.between(timeOfLastMedia, now)
 
-        if (allClientsMuted) {
-            val timeSinceAllClientsMuted = Duration.between(firstTimeAllClientsMuted!!, now)
-            return if (timeSinceAllClientsMuted > ALL_MUTED_TIMEOUT) {
-                SeleniumEvent.CallEmpty
-            } else {
-                null
-            }
-        } else if (timeSinceLastMedia > NO_MEDIA_TIMEOUT) {
-            return SeleniumEvent.NoMediaReceived
+        // There are a couple possible outcomes here:
+        // 1) All clients are muted, but have been muted for longer than ALL_MUTED_TIMEOUT so
+        //     we'll exit the call gracefully (CallEmpty)
+        // 2) No media has flowed for longer than NO_MEDIA_TIMEOUT and all clients are not
+        //     muted so we'll exit with an error (NoMediaReceived)
+        // 3) If neither of the above are true, we're fine and no event has occurred
+        return when {
+            clientsAllMutedTransitionTime.exceededTimeout(ALL_MUTED_TIMEOUT) -> SeleniumEvent.CallEmpty
+            timeSinceLastMedia > NO_MEDIA_TIMEOUT && !allClientsMuted -> SeleniumEvent.NoMediaReceived
+            else -> null
         }
-
-        return null
     }
 
     companion object {
@@ -73,3 +63,28 @@ class MediaReceivedStatusCheck(
         private val ALL_MUTED_TIMEOUT: Duration = Duration.ofMinutes(10)
     }
 }
+
+/**
+ * Track the most recent timestamp at which we transitioned from
+ * an event having not occurred to when it did occur.  Note this
+ * tracks the timestamp of that *transition*, not the most recent
+ * time the event itself occurred.
+ */
+private class StateTransitionTimeTracker(private val clock: Clock) {
+    private var timestampTransitionOccured: Instant? = null
+
+    fun maybeUpdate(eventOccurred: Boolean) {
+        if (eventOccurred && timestampTransitionOccured == null) {
+            timestampTransitionOccured = clock.instant()
+        } else if (!eventOccurred) {
+            timestampTransitionOccured = null
+        }
+    }
+
+    fun exceededTimeout(timeout: Duration): Boolean {
+        return timestampTransitionOccured?.let {
+            Duration.between(it, clock.instant()) > timeout
+        } ?: false
+    }
+}
+
