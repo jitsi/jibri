e9fa56a8049ed6317f3e69ad9860845574dd6831
Дамян Минков <damencho@jitsi.org>
2019-08-21 18:10:53 +0100

    Uses new versioning and tagging based on pom version and git describe. (#212)
    * Uses new versioning and tagging based on pom version and git describe.
    
    * Changes the version of deb to contain the source hash.

--- resources/jenkins/release.sh
+++ resources/jenkins/release.sh
@@ -3,43 +3,36 @@ set -e
 
 # This script should be run after the build/tests have successfully completed
 
-MAJOR_MINOR=$1
-
 # Prune any local tags that aren't on the remote
 git fetch --prune origin "+refs/tags/*:refs/tags/*"
 
-DESCRIBE=`git describe --tags --always`
-REV=$(git log --pretty=format:'%h' -n 1)
-
-CURR_VERSION=`echo $DESCRIBE | awk '{split($0,a,"-"); print a[1]}'`
-echo "Got current git version $CURR_VERSION"
-MAJOR_VERSION=`echo $CURR_VERSION | awk '{split($0,a,"."); print a[1]}'`
-MINOR_VERSION=`echo $CURR_VERSION | awk '{split($0,a,"."); print a[2]}'`
-
-if [ "$MAJOR_MINOR" == "Minor" ]; then
-    MINOR_VERSION=$((MINOR_VERSION+1))
-elif [ "$MAJOR_MINOR" == "Major" ]; then
-    MAJOR_VERSION=$((MAJOR_VERSION+1))
-    MINOR_VERSION=0
-else
-    echo "Error, unrecognized release type $MAJOR_MINOR.  Options are 'Major' and 'Minor'"
-    exit 1
-fi
-
-NEW_VERSION=$MAJOR_VERSION.$MINOR_VERSION
-echo "New version will be $NEW_VERSION"
-
 cd $WORKSPACE
 
-git tag -a $NEW_VERSION -m "New $MAJOR_MINOR release"
+# Let's get version from maven
+MVNVER=`xmllint --xpath "/*[local-name()='project']/*[local-name()='version']/text()" pom.xml`
+TAG_NAME="v${MVNVER/-SNAPSHOT/}"
+echo "Current tag name: $TAG_NAME"
 
-dch -v "$NEW_VERSION.$BUILD_NUMBER-1" "Built from git. $REV"
+if ! git rev-parse $TAG_NAME >/dev/null 2>&1
+then
+    git tag -a $TAG_NAME -m "Tagged automatically by Jenkins"
+	  git push origin $TAG_NAME
+else
+	echo "Tag: $TAG_NAME already exists."
+fi
+
+VERSION_FULL=`git describe --match "v[0-9\.]*" --long`
+echo "Full version: ${VERSION_FULL}"
+
+VERSION=${VERSION_FULL:1}
+echo "Package version: ${VERSION}"
+
+REV=$(git log --pretty=format:'%h' -n 1)
+
+# bulding the debian package expects the file target/jibri.jar
+mv target/jibri-${MVNVER}-jar-with-dependencies.jar target/jibri.jar
+
+dch -v "${VERSION}-1" "Built from git. $REV"
 dch -D unstable -r ""
 
 dpkg-buildpackage -A -rfakeroot -us -uc
-
-ARTIFACTS_DIR=$WORKSPACE/$BUILD_NUMBER-ARTIFACTS
-mkdir $ARTIFACTS_DIR
-mv $WORKSPACE/../jibri_$NEW_VERSION* $ARTIFACTS_DIR
-
-git push origin $NEW_VERSION
