b90541699b085d44fb9a43aeff45b951f9f06898
Brian Baldino <bbaldino@gmail.com>
2021-03-16 11:14:11 -0700

    move jibri id config prop out to a config class

--- src/main/kotlin/org/jitsi/jibri/Main.kt
+++ src/main/kotlin/org/jitsi/jibri/Main.kt
@@ -39,6 +39,7 @@ import org.jitsi.metaconfig.ConfigException
 import org.jitsi.metaconfig.MapConfigSource
 import org.jitsi.metaconfig.MetaconfigLogger
 import org.jitsi.metaconfig.MetaconfigSettings
+import org.jitsi.metaconfig.config
 import org.jitsi.metaconfig.configSupplier
 import java.io.File
 import java.util.concurrent.TimeUnit
@@ -66,15 +67,11 @@ fun main(args: Array<String>) {
             }
         }
     }
-    val jibriId = configSupplier<String> {
-        "JibriConfig::jibriId" { Config.legacyConfigSource.jibriId!! }
-        "jibri.id".from(Config.configSource)
-    }.get()
     val webhookSubscribers = configSupplier<List<String>> {
         "jibri.webhook.subscribers".from(Config.configSource)
     }.get()
 
-    val webhookClient = WebhookClient(jibriId)
+    val webhookClient = WebhookClient(MainConfig.jibriId)
 
     jibriStatusManager.addStatusHandler {
         webhookClient.updateStatus(it)
@@ -158,6 +155,16 @@ fun main(args: Array<String>) {
         }
     }.start()
 }
+
+class MainConfig {
+    companion object {
+        val jibriId: String by config {
+            "JibriConfig::jibriId" { Config.legacyConfigSource.jibriId!! }
+            "jibri.id".from(Config.configSource)
+        }
+    }
+}
+
 
 private fun handleCommandLineArgs(args: Array<String>) {
     val argParser = ArgumentParsers.newFor("Jibri").build()
