98f57e816950940ab0f6b0ae75de092f2c9f3229
Brian Baldino <bbaldino@gmail.com>
2021-03-24 13:56:15 -0700

    use jitsi-utils logger in jibrimanager

--- src/main/kotlin/org/jitsi/jibri/JibriManager.kt
+++ src/main/kotlin/org/jitsi/jibri/JibriManager.kt
@@ -44,12 +44,11 @@ import org.jitsi.jibri.status.ComponentState
 import org.jitsi.jibri.status.ErrorScope
 import org.jitsi.jibri.util.StatusPublisher
 import org.jitsi.jibri.util.TaskPools
-import org.jitsi.jibri.util.extensions.error
 import org.jitsi.jibri.util.extensions.schedule
 import org.jitsi.metaconfig.config
+import org.jitsi.utils.logging2.createLogger
 import java.util.concurrent.ScheduledFuture
 import java.util.concurrent.TimeUnit
-import java.util.logging.Logger
 
 class JibriBusyException : Exception()
 
@@ -84,7 +83,7 @@ data class FileRecordingRequestParams(
  * ComponentBusyStatus and ComponentState and i was unable to think of a better solution for that (yet...)
  */
 class JibriManager : StatusPublisher<Any>() {
-    private val logger = Logger.getLogger(this::class.qualifiedName)
+    private val logger = createLogger()
     private var currentActiveService: JibriService? = null
     /**
      * Store some arbitrary context optionally sent in the start service request so that we can report it in our
@@ -230,7 +229,7 @@ class JibriManager : StatusPublisher<Any>() {
                     try {
                         stopService()
                     } catch (t: Throwable) {
-                        logger.error("Error while stopping service due to usage timeout: $t")
+                        logger.error("Error while stopping service due to usage timeout", t)
                     }
                 }
         }
