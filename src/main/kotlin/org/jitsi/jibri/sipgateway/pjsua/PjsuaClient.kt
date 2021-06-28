88dd8fe08c2ace118c049b027120ae5d41333cdf
raluca8x8 <raluca.tocmag@8x8.com>
2021-06-28 09:43:43 +0300

    Do not rotate pjsua logs for now, as this can result in blocking the stdout writing of pjsua (#418)
    Co-authored-by: raluca.toth@8x8.com <raluca.toth@8x8.com>

--- src/main/kotlin/org/jitsi/jibri/sipgateway/pjsua/PjsuaClient.kt
+++ src/main/kotlin/org/jitsi/jibri/sipgateway/pjsua/PjsuaClient.kt
@@ -21,12 +21,10 @@ import org.jitsi.jibri.config.Config
 import org.jitsi.jibri.sipgateway.SipClient
 import org.jitsi.jibri.sipgateway.SipClientParams
 import org.jitsi.jibri.sipgateway.pjsua.util.PjsuaExitedPrematurely
-import org.jitsi.jibri.sipgateway.pjsua.util.PjsuaFileHandler
 import org.jitsi.jibri.sipgateway.pjsua.util.RemoteSipClientBusy
 import org.jitsi.jibri.status.ComponentState
 import org.jitsi.jibri.util.JibriSubprocess
 import org.jitsi.jibri.util.ProcessExited
-import org.jitsi.jibri.util.getLoggerWithHandler
 import org.jitsi.metaconfig.from
 import org.jitsi.metaconfig.optionalconfig
 import org.jitsi.utils.logging2.Logger
@@ -44,14 +42,13 @@ class PjsuaClient(
     private val pjsuaClientParams: PjsuaClientParams
 ) : SipClient() {
     private val logger = createChildLogger(parentLogger)
-    private val pjsua: JibriSubprocess = JibriSubprocess(logger, "pjsua", pjsuaOutputLogger)
+    private val pjsua: JibriSubprocess = JibriSubprocess(logger, "pjsua")
     private val sipOutboundPrefix: String? by optionalconfig(
         "jibri.sip.outbound-prefix".from(Config.configSource)
     )
 
     companion object {
         const val COMPONENT_ID = "Pjsua"
-        private val pjsuaOutputLogger = getLoggerWithHandler("pjsua", PjsuaFileHandler())
     }
 
     init {
