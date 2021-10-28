9c0596b6b6c7dc388c6d6a477d7008f5cdf461cf
emrah <emrah.com@gmail.com>
2021-10-28 14:41:45 +0300

    fix: get the internal-api-port from jibri.conf if exists

--- resources/debian-package/opt/jitsi/jibri/reload.sh
+++ resources/debian-package/opt/jitsi/jibri/reload.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-curl -X POST http://127.0.0.1:3333/jibri/api/internal/v1.0/notifyConfigChanged
+CONF="/etc/jitsi/jibri/jibri.conf"
+PORT=$(egrep -so "^\s*internal-api-port\s*=\s*[0-9]+" $CONF | \
+    egrep -so "[0-9]+" | tail -1)
+[[ -z "$PORT" ]] && PORT=3333
+
+curl -X POST http://127.0.0.1:$PORT/jibri/api/internal/v1.0/notifyConfigChanged
