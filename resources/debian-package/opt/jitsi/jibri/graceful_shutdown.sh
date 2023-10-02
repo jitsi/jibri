8d1f2bce24900c13f5077bddc51b5508619e3984
emrah <emrah.eryilmaz@nordeck.net>
2023-10-02 12:29:03 +0300

    refactor: dont show curl progress in jibri log

--- resources/debian-package/opt/jitsi/jibri/graceful_shutdown.sh
+++ resources/debian-package/opt/jitsi/jibri/graceful_shutdown.sh
@@ -5,4 +5,4 @@ CONF="/etc/jitsi/jibri/jibri.conf"
 PORT=$(hocon -f $CONF get jibri.api.http.internal-api-port 2>/dev/null || true)
 [[ -z "$PORT" ]] && PORT=3333
 
-curl -X POST http://127.0.0.1:$PORT/jibri/api/internal/v1.0/gracefulShutdown
+curl -sX POST http://127.0.0.1:$PORT/jibri/api/internal/v1.0/gracefulShutdown
