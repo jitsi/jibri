0271286b4c9dfbe5807a835bd1244e919be47f39
bgrozev <boris@jitsi.org>
2024-12-11 11:58:55 -0600

    chore: Adapt to jicoco changes. (#554)
    * chore: Adapt to jicoco changes.
    
    * chore: Update jicoco.

--- src/main/kotlin/org/jitsi/jibri/api/http/HttpApi.kt
+++ src/main/kotlin/org/jitsi/jibri/api/http/HttpApi.kt
@@ -21,7 +21,6 @@ import io.ktor.http.HttpStatusCode
 import io.ktor.http.parseHeaderValue
 import io.ktor.serialization.jackson.jackson
 import io.ktor.server.application.Application
-import io.ktor.server.application.call
 import io.ktor.server.application.install
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.server.request.receive
@@ -31,7 +30,6 @@ import io.ktor.server.routing.get
 import io.ktor.server.routing.post
 import io.ktor.server.routing.route
 import io.ktor.server.routing.routing
-import jakarta.ws.rs.core.Response
 import org.jitsi.jibri.FileRecordingRequestParams
 import org.jitsi.jibri.JibriBusyException
 import org.jitsi.jibri.JibriManager
