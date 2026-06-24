5901d6d0076f187d1f1c951bfad90401d70b1119
Hugo Lavernhe <hugo.lavernhe@gmail.com>
2026-06-24 13:57:04 +0200

    Update ExternalAPIPageTest.kt

--- src/test/kotlin/org/jitsi/jibri/selenium/pageobjects/ExternalAPIPageTest.kt
+++ src/test/kotlin/org/jitsi/jibri/selenium/pageobjects/ExternalAPIPageTest.kt
@@ -5,7 +5,6 @@ import io.kotest.core.spec.style.ShouldSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.mockk
-import io.mockk.verify
 import org.jitsi.jibri.CallUrlInfo
 import org.openqa.selenium.remote.RemoteWebDriver
 
@@ -17,24 +16,42 @@ internal class ExternalAPIPageTest : ShouldSpec() {
         val page = ExternalAPIPage(driver)
         val callUrl = CallUrlInfo("https://meet.example.com", "testroom")
 
-        should("return true when visiting call URL") {
+        should("return true when conference joined") {
             every { driver.get(any<String>()) } returns Unit
+            every { driver.executeScript(any<String>()) } returns true
+
             page.visit(callUrl) shouldBe true
-            verify { driver.get(any()) }
+        }
+
+        should("return false on timeout when conference never joins") {
+            every { driver.get(any<String>()) } returns Unit
+            every { driver.executeScript(any<String>()) } returns false
+
+            page.visit(callUrl) shouldBe false
+        }
+
+        should("return false if exception occurs") {
+            every { driver.get(any<String>()) } returns Unit
+            every { driver.executeScript(any<String>()) } throws RuntimeException("Script error")
+
+            page.visit(callUrl) shouldBe false
         }
 
         should("return participant count") {
             every { driver.executeScript(any<String>()) } returns 3L
+
             page.getNumParticipants() shouldBe 3
         }
 
         should("return true when only recorder present") {
             every { driver.executeScript(any<String>()) } returns 1L
+
             page.isCallEmpty() shouldBe true
         }
 
         should("return false when participants present") {
             every { driver.executeScript(any<String>()) } returns 2L
+
             page.isCallEmpty() shouldBe false
         }
     }
